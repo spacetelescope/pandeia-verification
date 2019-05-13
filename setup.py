e2254c2982acc60046e27d61da957b43962240f3
Adric Riedel <adric.riedel@gmail.com>
2019-05-13 18:08:07 -0400

    New v1.4 files and setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pandeia_verification',
-      version='1.0',
+      version='1.4',
       description='Pandeia verification tools',
       author='Klaus Pontoppidan',
       author_email='pontoppi@stsci.edu',
