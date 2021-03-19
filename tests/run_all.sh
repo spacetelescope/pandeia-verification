94f9d9fcd5af9efb712d46217c72e6dcd579c6dd
Adric Riedel <adric.riedel@gmail.com>
2021-03-19 20:02:51 +0000

    Enhance the test runner

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+# https://stackoverflow.com/questions/18622907/only-mkdir-if-it-does-not-exist
+mkdir -p ../logs
+mkdir -p ../outputs
 cd niriss/
 python niriss_sensitivity_soss.py > ../../logs/niriss_soss.log &
 cd ../miri/
 python miri_sensitivity_mrs.py > ../../logs/miri_mrs.log &
-python miri_sensitivity_mrs_extended.py > ../../logs/miri_mrs_extended.log &
 cd ../nirspec
 python nirspec_sensitivity_ifu.py > ../../logs/nirspec_ifu.log &
 cd ../niriss/
@@ -12,6 +15,7 @@ python niriss_sensitivity_imaging.py > ../../logs/niriss_imaging.log
 cd ../miri/
 python miri_sensitivity_imaging.py > ../../logs/miri_imaging.log
 python miri_sensitivity_imaging_extended.py > ../../logs/miri_imaging_extended.log
+python miri_sensitivity_mrs_extended.py > ../../logs/miri_mrs_extended.log 
 python miri_sensitivity_lrs.py > ../../logs/miri_lrs.log
 cd ../nirspec
 python nirspec_sensitivity_fs.py > ../../logs/nirspec_fs.log
