d46b134ad1b332e769ab3bc1dea57aaed6d420c7
Adric Riedel <adric.riedel@gmail.com>
2023-03-28 09:39:21 -0400

    We also need to specify these dithers

--- tests/miri/miri_sensitivity_mrs.py
+++ tests/miri/miri_sensitivity_mrs.py
@@ -33,8 +33,8 @@ exp_config = {
 strategy = {
             'target_xy': [0.0, 0.0],
             'method': 'ifunodinscene',
-            'aperture_size': 1.1#,
-            #'dithers': [{'x':0,'y':0},{'x':1,'y':1}]
+            'aperture_size': 1.1,
+            'dithers': [{'x':0,'y':0},{'x':1,'y':1}]
             }
 
 outputs_regular, outputs_one = calc_limits.calc_limits(configs,apertures,idt_fluxes,obsmode=obsmode,scanfac=150,skyfacs=1.05,
