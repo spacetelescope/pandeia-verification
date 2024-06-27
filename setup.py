47c2afa55deaf8af56adce6d10c026e13e4e194f
Adric Riedel <adric.riedel@gmail.com>
2024-06-27 12:21:05 -0400

    fix the installer

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='pandeia_verification',
       author_email='pontoppi@stsci.edu',
       url='http://www.stsci.edu/~pontoppi',
       packages=find_packages(),
-      package_data={'verification_tools': ['inputs/*.fits','inputs/*.txt','inputs/*.tab', 'tests/niriss/*.py', 'tests/nircam/*.py', 'tests/miri/*.py', 'tests/nirspec/*.py']}
+      package_data={'verification_tools': ['inputs/*.fits','inputs/*.txt','inputs/*.tab', 'tests/niriss/*.py', 'tests/nircam/*.py', 'tests/miri/*.py', 'tests/nirspec/*.py']},
       install_requires = [
           "astropy",
           "bokeh",
